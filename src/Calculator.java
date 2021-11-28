709410a96db108895dbd4d405fae67d9f51fd93f
arsenn121 <arsen.sarkisian@hneu.net>
2021-11-28 18:18:06 +0200

    Main

---
+++ src/Calculator.java
@@ -0,0 +1,10 @@
+class Calculator {
+    public double num1, num2;
+    public Calculator(){}
+    public void setNum1(double num1) {this.num1 = num1;}
+    public void setNum2(double num2) {this.num2 = num2;}
+    public double getNum1(){return num1;}
+    public double getNum2(){return num2;}
+
+
+}

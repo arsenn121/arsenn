7dd79f8947a939cfe6819bd89cf4c0e50947cb7e
yelyzaveta060503 <arsen.sarkisian@hneu.net>
2021-11-28 21:02:55 +0200

    dop operacia

--- src/AdvancedCalc.java
+++ src/AdvancedCalc.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class AdvancedCalc {
+
+class AdvancedCalc extends Calculator {
+
+    public AdvancedCalc(){}
+
+    public  double pow() {
+        return Math.sin(num1);
+    }
+
+
 }

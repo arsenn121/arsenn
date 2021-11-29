faabe0a4ed434bda163b0efabffb82d96ef9a1b4
yelyzaveta060503 <95128458+yelyzaveta060503@users.noreply.github.com>
2021-11-29 10:18:17 +0200

    Update AdvancedCalc.java

--- src/AdvancedCalc.java
+++ src/AdvancedCalc.java
@@ -10,3 +10,10 @@ class AdvancedCalc extends Calculator {
 
 
 }
+   public static void enterData() {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        double num1,num2;
+        System.out.println("Выберите режим: 1 или 2");
+        int userOption = sc.nextInt();
+        AdvancedCalc cal2 = new AdvancedCalc();
+        Calculator cal1 = new Calculator();

92c4664652c367a328d20b9de334edff760b6199
yelyzaveta060503 <95128458+yelyzaveta060503@users.noreply.github.com>
2021-11-29 10:19:13 +0200

    Update AdvancedCalc.java

--- src/AdvancedCalc.java
+++ src/AdvancedCalc.java
@@ -17,3 +17,19 @@ class AdvancedCalc extends Calculator {
         int userOption = sc.nextInt();
         AdvancedCalc cal2 = new AdvancedCalc();
         Calculator cal1 = new Calculator();
+        switch (userOption) {
+            case 1:
+                System.out.println("\n---------------------------------------");
+                System.out.print("Выберите операцию: \n1. \"+\" \n2. \"/\" \n3. \"-\" \n4. \"*\" \n");
+                System.out.println("---------------------------------------");
+                int userOption2 = sc.nextInt();
+                while (true) {
+                    System.out.println("введите первое число");
+                    num1 = sc.nextDouble();
+                    System.out.println("введите второе число");
+                    num2 = sc.nextDouble();
+                    cal1.setNum1(num1);
+                    cal1.setNum2(num2);
+                    if (userOption2!=2) {
+                        break;
+                    }
